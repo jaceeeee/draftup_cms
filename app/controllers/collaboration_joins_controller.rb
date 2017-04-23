947189520dd7f1446cdbdce1ce45050647468c3c
Jace <jtroldan@up.edu.ph>
2017-04-23 19:36:21 +0800

    functional, add Collaborations

--- app/controllers/collaboration_joins_controller.rb
+++ app/controllers/collaboration_joins_controller.rb
@@ -1,20 +1,27 @@
+# used for linking a student to other students through Collaborations
 class CollaborationJoinsController < ApplicationController
   layout 'app_theme'
   before_action :find_student
+  # @student is instantiated at every method call
 
+# displays all collaborations regardless of who is in it
   def index
     @collaborations = Collaboration.all
   end
 
+# finds the collaboration corresponding to id
   def edit
     @collaboration = Collaboration.find(params[:id])
   end
 
+# updates the collaboration by adding it to the logged-in student
   def update_collaboration
     @collaboration = Collaboration.find(params[:id])
     if @student.collaborations << @collaboration
+      # redirects to home page
       redirect_to admin_path
     else
+      # rerenders the prompt page
       render 'edit'
     end
   end
--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,46 +1,59 @@
 class CollaborationsController < ApplicationController
   layout 'app_theme'
-# still empty; needs tweaking
   before_action :find_student
+  # finds a student at each method call
 
+  # finds all collaborations belonging to a student
   def index
     @collaborations = @student.collaborations.all
   end
 
+  # finds a specific collaboration and renders the show template
   def show
     @collaboration = @student.collaborations.find(params[:id])
   end
 
+  # initializes an empty collaboration
   def new
     @collaboration = Collaboration.new
   end
 
+  # creates and saves the student with current parameters
   def create
     @collaboration = Collaboration.new(collaboration_params)
     if @student.collaborations << @collaboration
+      # redirects to index
       redirect_to(collaborations_path)
     else
+      # rerenders the form
       render 'new'
     end
   end
 
+  # shows the edit form template filled in with params of
+  # selected collaboration
   def edit
     @collaboration = Collaboration.find(params[:id])
   end
 
+  # updates the collaboration
   def update
     @collaboration = Collaboration.find(params[:id])
     if @collaboration.update_attributes(collaboration_params)
+      # redirects to 'show' template
       redirect_to(collaboration_path(@collaboration))
     else
+      # rerenders 'edit' template
       render 'edit'
     end
   end
 
+  # renders 'delete' template for selected thesis
   def delete
     @collaboration = Collaboration.find(params[:id])
   end
 
+  # destroys selected thesis object from the database
   def destroy
     @collaboration = Collaboration.find(params[:id])
     @collaboration.destroy
